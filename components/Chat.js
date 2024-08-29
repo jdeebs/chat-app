5092ac850af7cecbc350332947259da613f721ba
Jacob DiBiase <jakesback93@hotmail.com>
2024-08-29 05:56:10 -0700

    Fix SDK compatibility error and update Chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,36 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, background } = route.params;
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
+  // Set the messages state with a static message
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: `Hello ${name}`,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
 
   // Set the title of the screen to the user's name or "Chat" if no name is provided
   useEffect(() => {
@@ -16,7 +44,17 @@ const Chat = ({ route, navigation }) => {
   // Render component with dynamic background color passed from Start.js
   return (
     <View style={[styles.container, { backgroundColor: background }]}>
-      <Text style={styles.text}>Hello {name || "there"}!</Text>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+        style={{ flex: 1}}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
     </View>
   );
 };
@@ -24,13 +62,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    fontSize: 32,
-    fontWeight: "bold",
-    color: "#FFF",
   },
 });
 
